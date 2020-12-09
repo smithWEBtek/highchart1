d0e8010ff28b289b8f3e3450debfee0b1f19d01f
smithwebtek <brad@smithwebtek.com>
2020-12-09 15:06:13 -0500

    Tweaks

--- app/javascript/packs/fruits.js
+++ app/javascript/packs/fruits.js
@@ -1,13 +1,21 @@
+import moment from 'moment'
+
 document.addEventListener('DOMContentLoaded', function () {
+  let d1 = new Date("2020-12-03")
+  let d2 = new Date("2020-12-07")
+  let d3 = new Date("2020-12-11")
+
   const chart = Highcharts.chart('container', {
+
     chart: {
-      type: 'bar'
+      type: 'column'
     },
     title: {
       text: 'Fruit Consumption'
     },
     xAxis: {
-      categories: ['Apples', 'Bananas', 'Oranges']
+      // categories: ['Apples', 'Bananas', 'Oranges']
+      dates: [d1, d2, d3]
     },
     yAxis: {
       title: {
@@ -16,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
     },
     series: [{
       name: 'Jane',
-      data: [1, 0, 4]
+      data: [1, 3, 4]
     }, {
       name: 'John',
       data: [5, 7, 3]
