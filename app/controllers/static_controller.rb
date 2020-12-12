4c18eb3bcc50badf3cbfd1cfe2c7be0a6cdbbf31
smithwebtek <brad@smithwebtek.com>
2020-12-12 08:30:06 -0500

    Convert to newsapis focus

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,14 +1,6 @@
 class StaticController < ApplicationController
 
-  def fruits
-    render :fruits
-  end
-
-  def stocks
-    render :stocks
-  end
-
-  def temp
-    render :temp
+  def charts
+    render :charts
   end
 end
