cdbc2c353a47cc2334983b10c1fd8d29618ea837
smithwebtek <brad@smithwebtek.com>
2020-12-16 14:29:16 -0500

    Update nyt

--- app/javascript/services/apiNytimesArticleSearch.js
+++ app/javascript/services/apiNytimesArticleSearch.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log("the apiNytimesArticleSearch js file loaded")
 
   fetchArticles()
+  debugger;
 })
+const API_KEY = process.env["NYT_API_KEY"]
+// const API_KEY = process.env.NYT_API_KEY
 
 const fetchArticles = () => {
-
-  let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=ZSUEMQxulNj8dK5AkSjdlJGLwiiJAjws"
+  let url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=arts&api-key=${API_KEY}`
 
   $.ajax({
     url: url,
