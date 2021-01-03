5edbe81d86d6099282526fef7539cc84945b4839
smithwebtek <brad@smithwebtek.com>
2021-01-03 12:55:11 -0500

    Branch to try all one svg

--- app/javascript/backgammon/backgammon.js
+++ app/javascript/backgammon/backgammon.js
@@ -3,25 +3,28 @@ document.addEventListener("DOMContentLoaded", () => {
   setupBoard();
 });
 
+tokenTest = () => {
+  return (`<span class="token-black"></span>`)
+}
+
 tokenBlack = () => {
-  return (`
-    <svg xmlns="http://www.w3.org/2000/svg">
-      <circle cx="60" cy="100" r="23" fill="black" opacity="1"/>
-    </svg>
-  `);
-};
+  return (`<svg><circle cx="60" cy="50" r="23" fill="black" class="token-black" opacity="1" /></svg>`)
+}
 
 const tokenWhite = () => {
   return `
     <svg height="100" width="100">
-      <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="lightbrownd" />
+      <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="lightbrown" />
     </svg>
   `;
 };
 
 const setupBoard = () => {
   $("button#setup-board").on("click", () => {
-    $('div#tokens').append(tokenBlack())
+    // $('div#tokens').append(tokenBlack())
+    // $('polygon#13.triange-black').html(tokenBlack())
+    // $('svg polygon#13').html(`<span class="token-black"></span>`)
+    $('svg polygon#13').prepend(tokenTest())
   });
 };
 
